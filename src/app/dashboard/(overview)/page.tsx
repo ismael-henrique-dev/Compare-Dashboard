13ac7add8a3a9e65f2110c41cced9ff0a5a4d29d
ismael-henrique-dev <ismael.henrique.dev@gmail.com>
2025-12-29 13:35:20 -0300

    [M1-01/01-2] feat: create the home page and add UI components by shadcn

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,3 +1,51 @@
-export default function Home() {
-  return <span>Home</span>
+import { SiteHeader } from '@/components/core/site-header'
+import { ScrapingSummaryChart } from '@/components/home/chart'
+import { RecentCollections } from '@/components/home/recent-collections'
+import { SummaryCards } from '@/components/home/summary'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from '@/components/ui/breadcrumb'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Home',
+}
+
+export default function Dashboard() {
+  return (
+    <>
+      <SiteHeader>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbPage>Dashboard</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </SiteHeader>
+      <div className='flex flex-1 flex-col p-5 gap-6'>
+        {/* <div className='@container/main flex flex-1 flex-col gap-2'> */}
+        {/* <div className='flex flex-col gap-4 py-4 md:gap-6 md:py-6'> */}
+        <SummaryCards />
+        <div className='space-y-6'>
+          <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
+            {/* O gráfico ocupa 2 colunas */}
+            <div className='lg:col-span-2'>
+              <ScrapingSummaryChart />
+            </div>
+
+            {/* A lista de coletas ocupa 1 coluna */}
+            <div className='lg:col-span-1'>
+              <RecentCollections />
+            </div>
+          </div>
+        </div>
+        {/* </div> */}
+        {/* </div> */}
+      </div>
+    </>
+  )
 }
