c04aa6f2d160dc552edd64e5882d3ed0b7c19e2a
ismael-henrique-dev <ismael.henrique.dev@gmail.com>
2026-01-15 16:04:52 -0300

    # fix: update the select store component on sraping erros page

--- src/components/scraping-erros/filters.tsx
+++ src/components/scraping-erros/filters.tsx
@@ -1,22 +1,23 @@
-"use client";
-import FilterShopSelect from "../ui/filters";
-import { Label } from "../ui/label";
-import { DatePicker } from "./date-picker";
+'use client'
+
+import { SelectStores } from '../ui/filters'
+import { Label } from '../ui/label'
+import { DatePicker } from './date-picker'
 
 function ScrapingErrorsFilters() {
   return (
-    <div className="grid grid-cols-[1fr_auto] gap-2">
-      <div className="grid gap-1">
+    <div className='grid grid-cols-[1fr_auto] gap-2'>
+      <div className='grid gap-1'>
         <Label>Loja</Label>
-        <FilterShopSelect />
+        <SelectStores />
       </div>
 
-      <div className="grid gap-1 lg:col-span-1 col-span-2">
+      <div className='grid gap-1 lg:col-span-1 col-span-2'>
         <Label>Data</Label>
         <DatePicker />
       </div>
     </div>
-  );
+  )
 }
 
-export default ScrapingErrorsFilters;
+export default ScrapingErrorsFilters
